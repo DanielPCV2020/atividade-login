f1194242051d8d23b784a0958dffd32fb696de66
Daniel <daniel.netoacc@gmail.com>
2022-03-30 23:01:06 -0400

    projeto completo com imagens

---
+++ newProjectLogin/projeto/ProjetoLogin/firstProject/firstProject/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace firstProject
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        
+
+
+        }
+
+        private void btLogin_Click(object sender, RoutedEventArgs e)
+        {
+            if((boxSenha.Password == "123456")&& (txtUsuario.Text== "zeca"))
+            {
+                MessageBox.Show("acertou");
+
+                var window = new RegrasdeNegocios.CalculadoraWindow();
+                this.Close();
+                window.ShowDialog();
+               
+            }
+            else
+            {
+                MessageBox.Show("se lascou");
+            }
+        }
+
+        private void txtSair_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+
+        }
+
+        private void mini_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+    }
+}
---
+++ newProjectLogin/projeto/ProjetoLogin/firstProject/firstProject/RegrasdeNegocios/CalculadoraWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace firstProject.RegrasdeNegocios
+{
+    /// <summary>
+    /// Lógica interna para CalculadoraWindow.xaml
+    /// </summary>
+    public partial class CalculadoraWindow : Window
+    {
+        public CalculadoraWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            double peso = Convert.ToDouble(TxtBoxPeso.Text);
+            double altura = Convert.ToDouble(TxtBoxAltura.Text);
+            double calculo;
+            calculo = peso / (altura * altura);
+            if (calculo < 18.5)
+            {
+                MessageBox.Show("o resultado do IMC é " + calculo +" Abaixo do peso ideal");
+
+            }else if ((calculo >= 18.5) && (calculo <=24.9))
+            {
+                MessageBox.Show("o resultado do IMC é " + calculo + " peso ideal(normal)");
+            }
+            else if ((calculo >= 25) && (calculo <= 29.9))
+            {
+                MessageBox.Show("o resultado do IMC é " + calculo + " sobre peso");
+            }
+            else if ((calculo >= 30) && (calculo <= 34.9))
+            {
+                MessageBox.Show("o resultado do IMC é " + calculo + " Obesidade Grau 1");
+            }
+            else if ((calculo >= 35) && (calculo <= 39.9))
+            { 
+                MessageBox.Show("o resultado do IMC é " + calculo + " Obesidade Grau 2");
+            }
+            else if (calculo>40)
+            {
+                MessageBox.Show("o resultado do IMC é " + calculo + " Obesidade Grau 3 ou Mórbida");
+            }
+            
+
+
+        }
+    }
+}
